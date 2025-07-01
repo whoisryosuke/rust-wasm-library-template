88c1b712ef6c5ad6d09b83f1165473d7859832e0
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-07-01 15:20:14 -0700

    :construction: r2

--- src/lib.rs
+++ src/lib.rs
@@ -14,20 +14,20 @@ impl BitcrusherModule {
         BitcrusherModule { bits, phaser: 0.0, last: 0.0 }
     }
 
-    pub fn process(&mut self, samples: &[f32], normfreq: f32) -> Vec<f32> {
+    pub fn process(&mut self, samples: Vec<f32>, normfreq: f32) -> Vec<f32> {
+
+        let mut output = samples;
         
         let step = 0.5_f32.powf(self.bits as f32);
 
-        let mut output = Vec::new();
-
         // Loop over samples and apply bitcrusher effect
-        for sample in samples {
+        for sample in output.iter_mut() {
             self.phaser += normfreq;
             if self.phaser >= 1.0 {
                 self.phaser -= 1.0;
-                self.last = step * (sample / step + 0.5).floor(); 
+                self.last = step * (*sample / step + 0.5).floor(); 
             }
-            output.push(self.last);
+            *sample = self.last;
         }
 
         output
